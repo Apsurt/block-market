d8b2de648416784b084c7f4419730505d525288e
Apsurt <tymon@becella.eu>
2026-03-04 18:24:42 +0100

    Initial commit

---
+++ src/client/kotlin/com/apsurt/blockmarket/client/BlockMarketClient.kt
@@ -0,0 +1,9 @@
+package com.apsurt.blockmarket.client
+
+import net.fabricmc.api.ClientModInitializer
+
+class BlockMarketClient : ClientModInitializer {
+
+    override fun onInitializeClient() {
+    }
+}
---
+++ src/main/kotlin/com/apsurt/blockmarket/BlockMarket.kt
@@ -0,0 +1,9 @@
+package com.apsurt.blockmarket
+
+import net.fabricmc.api.ModInitializer
+
+class BlockMarket : ModInitializer {
+
+    override fun onInitialize() {
+    }
+}
